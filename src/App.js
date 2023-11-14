5bb7ccdcab522e5505d475858fba966e2f75f417
ahmad-ben <ahmadbenchakhti@gmail.com>
2023-11-14 19:11:21 +0100

    add, delete and check task features.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasksArray, setTasksArray] = useState([]);
+
+  const inputRef = useRef();
+
+  const addATask = () => {
+    if(!inputRef.current.value) return;
+    const newTaskObject = {taskText: inputRef.current.value, taskState: false};
+    setTasksArray([...tasksArray, newTaskObject]);
+    inputRef.current.value = "";
+  };
+
+  const taskClicked = (taskIndex) => {
+    let newTasksArray = [...tasksArray];
+    newTasksArray[taskIndex].taskState = !newTasksArray[taskIndex].taskState;
+
+    setTasksArray(newTasksArray);
+  }
+
+  const deleteTask = (taskIndex) => {
+    let newTasksArray = [...tasksArray];
+    newTasksArray.splice(taskIndex, 1);
+    setTasksArray(newTasksArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>To Do List</h2> 
+      <div class="tasksContainer">
+
+        <ul>
+          { tasksArray.map(({ taskText, taskState }, taskIndex) => {
+            return(
+              <div class="taskContainer">
+                <li 
+                  onClick={() => taskClicked(taskIndex)} 
+                  className={taskState ? "completedTask" : ""} 
+                  //! isn't best practice style={{textDecoration : taskState ? "line-through" : ""}}
+                  key={taskIndex}
+                >
+                  { taskText }
+                </li>
+                <span onClick={() => deleteTask(taskIndex)} > ❌ </span>
+              </div>
+            )
+          }) }
+        </ul>
+        <input ref={inputRef} placeholder='Enter A Task...' />
+        <button onClick={addATask} >Add</button>
+
+      </div>
     </div>
   );
 }
